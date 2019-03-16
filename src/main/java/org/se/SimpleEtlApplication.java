f30e09bda4c333c487de4beff200c4ef82bfc8f0
GaoYiyang <370625414@qq.com>
2019-03-16 12:14:29 +0800

    1

---
+++ src/main/java/org/se/SimpleEtlApplication.java
@@ -0,0 +1,13 @@
+package org.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/se/SimpleEtlApplicationTests.java
@@ -0,0 +1,16 @@
+package org.se;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleEtlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
